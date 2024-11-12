87d2cb85bcf5790d7c0366642576112b073cd262
RuinaImperii-dev <maksimpechin12785@gmail.com>
2024-11-12 19:45:44 +0400

    мидлак для ролей

--- main.go
+++ main.go
@@ -14,8 +14,18 @@ func InitMiddleware(url string) {
 	URL = url
 }
 
+type Role struct {
+	ID          int     `json:"id"`
+	Label       string  `json:"label"`
+	AccessLevel int     `json:"accessLevel"`
+	CreatedAt   string  `json:"createdAt"`
+	UpdatedAt   string  `json:"updatedAt"`
+	DeletedAt   *string `json:"deletedAt"`
+}
+
 type middleware struct {
-	UserId int `json:"userId"`
+	UserId int    `json:"userId"`
+	Roles  []Role `json:"roles"`
 }
 
 func MiddlewareUser(c *fiber.Ctx) error {
@@ -57,6 +67,85 @@ func MiddlewareUser(c *fiber.Ctx) error {
 
 	if err := json.Unmarshal([]byte(body), &middlewareStruct); err != nil {
 		return err
+	}
+
+	c.Locals("userId", middlewareStruct.UserId)
+
+	return c.Next()
+}
+
+func MiddlewareRole(roles []int, strict bool, c *fiber.Ctx) error {
+
+	req, err := http.NewRequest("POST", URL, nil)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	c.Request().Header.VisitAll(func(key, value []byte) {
+		req.Header.Set(string(key), string(value))
+	})
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 403 || resp.StatusCode == 401 {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"message": "Forbidden",
+		})
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	var middlewareStruct middleware
+
+	if err := json.Unmarshal([]byte(body), &middlewareStruct); err != nil {
+		return err
+	}
+
+	// Создаем типа объекта для хранения id ролей пользователя
+	userRoleIDs := make(map[int]bool)
+	for _, role := range middlewareStruct.Roles {
+		userRoleIDs[role.ID] = true
+	}
+
+	// Проверка ролей
+	hasRequiredRoles := false
+	if strict {
+		// Строгий режим,  все указанные роли должны быть
+		hasRequiredRoles = true
+		for _, roleId := range roles {
+			if !userRoleIDs[roleId] {
+				hasRequiredRoles = false
+				break
+			}
+		}
+	} else {
+		// Не строгий режим, хотя бы одна из указанных ролей должна быть
+		for _, roleId := range roles {
+			if userRoleIDs[roleId] {
+				hasRequiredRoles = true
+				break
+			}
+		}
+	}
+
+	if !hasRequiredRoles {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"message": "Forbidden",
+		})
 	}
 
 	c.Locals("userId", middlewareStruct.UserId)
